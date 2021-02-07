e467baf8cb551965dd26ac7a45d50f2e541c488f
Aletvia Lecona <aletvialecona@gmail.com>
2021-02-07 15:52:03 -0600

    Templates

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.views import View
+from django.views.generic import ListView
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .forms import loginForm
+
+
+class login_view(View):
+    def get (self, request):
+        form = loginForm()
+        contex={
+                "title":"Login",
+                "form": form
+                }
+        return render(request, 'landingpage/auth-login.html', contex)
+
+    def post (self, request):
+        return redirect('dashboard_view')
+
+
+class Dashboard_view(View):
+    def get (self, request):
+        contex={"title":"Inicios de sesión"}
+        return render(request, 'dashboard/index.html', contex)
+
+
+class recover_pass(View):
+    def post (self, request):
+        messages.success(request, 'Se ha enviado un correo electrónico a la dirección solicitada.')
+        return redirect('login_view')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login_view')
